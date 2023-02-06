4b601e1ca3298aad20eeba10b7d7405e1dea76d1
JacobChristen <123013058+JacobChristen@users.noreply.github.com>
2023-02-06 15:46:52 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,11 @@ Chassis chassis;
  * Q1 - Declare here all the variables that your code is using!
  */
 void setup() 
+  // SA = switch A 
+  // SB = switch B 
+  // M1 = Motor 1 , opposite side of SA
+  // M2 = Motor 2, opposite side of SB
+  // 
 {
 /*
  * Q2 - Add interrupts here for both sensors SA and SB. They should only work 
@@ -23,6 +28,24 @@ void setup()
  */
   Serial.begin(9600);
   chassis.init();
+ const byte interruptPin0 = 0
+   const byte interruptPin1 = 1
+   
+ void setup() {
+   pinMode(interruptPin, INPUT_PULLUP);
+   attachInterrupt(digitalPinToInterrupt(interruptPin0),blank, SA)
+     if (interruptPin)
+         M2 = 0 )
+       
+   attachInterrupt(digitalPinToInterrupt(interruptPin1),blank, SB)
+       if (interruptPin)
+            M1 = 0)
+       
+   
+  
+                   
+  
+  
 }
 
 
@@ -35,10 +58,16 @@ void loop()
  * Q3 - Your car should be moving straight with a speed of 100 on both wheels, until interrupted.
  */
 
+ while (Romi (interruptPin = false)
+        M1 = 100
+        M2 = 100)
+   if (Romi (interruptPin = true)
+       M1 = 0 
+       M2 = 0)
+        
+        
 
-  if (readBatteryMillivolts() <= 6000) {
-    Romi32U4ButtonC buttonC;
-    buttonC.waitForButton();
+ 
   }
 
 }
@@ -47,4 +76,15 @@ void loop()
  * Q4 - Add the functions that your interrupts will call (lets say, ISRsa and ISRsb).
  *  Inside each one of these functions you should have a conditional that "does nothing"
  *  if the interrupt was triggered less than 2000 ms ago.
+   
  */
+void loop()
+  ISRsa (if (interruptPin < 2000ms) {
+    "does nothing ")
+    }
+ ISRsb (if interruptPin < 2000ms) { 
+   "does nothing")
+   }
+ 
+         
+  
