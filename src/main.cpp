461529ff2cb96da5a26cf5cab8b9f05db65d589b
Andre <43994211+arosendo@users.noreply.github.com>
2023-02-06 14:08:33 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,49 @@
+/**
+ * 
+ * QUIZ 1 - Follow the instructions for Q1-Q5
+ * 
+ * */ 
+
 #include <Arduino.h>
 #include <wpi-32u4-lib.h>
 
 #include <Chassis.h>
 
+Chassis chassis;
 
-void checkRemote() {
-  int16_t code = decoder.getKeyCode();
-  switch (code) {
-    case LEFT_ARROW:
-    Serial.println("LEFT");
-    //Do something fancy here, or set a specific state!
-    //Other fancy things too!
-    //Adding
-    //New line
-    break;
-    case RIGHT_ARROW:
-    Serial.println("RIGHT");
-    //test
-    break;
-    case UP_ARROW:
-    Serial.println("UP");
-    //New line
-    break;
-    case VOLplus:
-    Serial.println("Volume plus");
-    //more changes
-    break;
-  }
-}
-
-void setup() {
+/*
+ * Q1 - Declare here all the variables that your code is using!
+ */
+void setup() 
+{
+/*
+ * Q2 - Add interrupts here for both sensors SA and SB. They should only work 
+ * when the button is pressed! They should also be set as pull ups.
+ */
   Serial.begin(9600);
-  decoder.init();
+  chassis.init();
 }
 
-void loop() {
-  checkRemote();
-  //Running fancy codes here!
+
+void loop()
+{
+//  chassis.setMotorEfforts(0,0);
+//  delay(1000);
+
+/*
+ * Q3 - Your car should be moving straight with a speed of 100 on both wheels, until interrupted.
+ */
+
+
+  if (readBatteryMillivolts() <= 6000) {
+    Romi32U4ButtonC buttonC;
+    buttonC.waitForButton();
+  }
+
 }
+
+/*
+ * Q4 - Add the functions that your interrupts will call (lets say, ISRsa and ISRsb).
+ *  Inside each one of these functions you should have a conditional that "does nothing"
+ *  if the interrupt was triggered less than 2000 ms ago.
+ */
